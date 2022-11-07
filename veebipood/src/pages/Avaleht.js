629c3b91dc9e7dabd76e0d3123714972ce8a70ba
LaurMaldis <117661481+LaurMaldis@users.noreply.github.com>
2022-11-07 20:28:41 +0200

    Routes, Route, Link, pages ja react-router-dom

---
+++ veebipood/src/pages/Avaleht.js
@@ -0,0 +1,11 @@
+
+function Avaleht() {
+    return ( 
+    <div>
+        <div>Oled avalehel</div>
+        <button>Nupuke</button> 
+    </div>
+    );
+}
+
+export default Avaleht;
---
+++ veebipood/src/pages/LisaToode.js
@@ -0,0 +1,6 @@
+
+function LisaToode() {
+    return ( <div>Oled tulnud toodete lisamise lehele</div> );
+}
+
+export default LisaToode;
