82d801009351167e1d50aefb654a6a43b249e5ee
LaurMaldis <117661481+LaurMaldis@users.noreply.github.com>
2022-11-08 23:17:50 +0200

    konna, nupu ja teksti lisamine

---
+++ kodutoo/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className='nupp'>NUPP</button>
+      <div className='tekst'>TEKST</div>
+      <img className='konn' src="https://i.pinimg.com/736x/1a/e7/33/1ae733509d7ba91b55b034230249f77b.jpg" alt="Pilt konnast" />
+    </div>
+  );
+}
+
+export default App;
